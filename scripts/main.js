4f09eef76307582bb52fb6525faae86373631aa1
MykolaDekusar <ndekusar97@gmail.com>
2024-05-06 13:57:04 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// - Creo una variabile array che contiene gli elementi della lista
+// - inizializzo un contantore
+// - creo un ciclo while che mi cicla per la lunghezza della lista
+// - dentro il ciclo creo gli elementi lista con createElement
+// - inizializzo una variabile li che continete l'elemento corrente dell'array
+// - vado ad inserire dentro una lista non ordinata gli elementi della lista
