567262fdf3cbbe3bb1a261272eab3b4d0e64232e
MykolaDekusar <ndekusar97@gmail.com>
2024-05-06 14:14:01 +0200

    [END]End of exercise

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,19 @@
 // - dentro il ciclo creo gli elementi lista con createElement
 // - inizializzo una variabile li che continete l'elemento corrente dell'array
 // - vado ad inserire dentro una lista non ordinata gli elementi della lista
+
+const lista = ['Chitarra', 'Processore', 'Bagnoschiuma', 'Vernice', 'Botte in PVC', 'Acido Fosforico'];
+//inizializzo il contatore
+let contatore = 0;
+
+//creo il ciclo while
+while (contatore < lista.length) {
+    const liSta = document.createElement('li');//creo l'elemento lista
+    liSta.innerText = lista[contatore]; //inserisco dentro all'elemento lista il testo della mia lista
+    const ul = document.getElementById('spesa');//seleziono l'ul tramite id
+    ul.append(liSta);//"appendo" l'elemento li all'elemento ul
+    contatore++;//aumento il contatore per uscire fuori dal loop
+    console.log(contatore);
+}
+console.log(contatore);
+console.log(lista);
